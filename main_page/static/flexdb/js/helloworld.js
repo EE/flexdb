34540a4b5ed3f4d8032d407be5283e524240f124
Cezar <cezar@rome>
2013-08-20 14:43:06 +0200

    first add

--- main_page/static/flexdb/js/helloworld.js
+++ main_page/static/flexdb/js/helloworld.js
@@ -1,30 +1,76 @@
-angular.module('flexdb', []).
-  config(function($interpolateProvider){
+var myApp = angular.module('flexdb', []).
+  config(function($interpolateProvider, $httpProvider){
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
   });
 
 
-function tableControll ($scope){
-    $scope.users = [
-        {Lname:'Walt', Fname:'Kowalsky', Email: 'Walt@walt.org', Country: 'USA', Addres: 'New Jersey' }
-        ];
+function tableControll ($scope, $http){
 
+    $scope.data = {};
+    $scope.data.Logged = false;
+    $scope.user = {};
+    $scope.userid = -1;
+    $scope.is_user_data_shown = false;
 
-    $scope.addUser = function() {
-        $scope.users.push({
-            Lname:$scope.Lname,
-            Fname:$scope.Fname,
-            Email:$scope.Email,
-            Country:$scope.Country,
-            Addres:$scope.Addres
+    function getID () {
+        $http({
+            method: 'GET',
+            url: '/mainPageAjax/getUserData'
+        }).success( function  ajaxDone (data) {
+            if (data.Logged != null){
+                $scope.data.Logged = data.Logged;
+
+                if (data.userid != null){
+                    $scope.userid = data.userid;
+
+                }
+            }
         });
+    }
 
-        $scope.Lname = '';
-        $scope.Fname = '';
-        $scope.Email = '';
-        $scope.Country = '';
-        $scope.Addres = '';
-    };
+    function ajaxDone(data) {
+            if (data.username != null) {
+                $scope.data.username = data.username;
+                $scope.user.username = data.username;
+            }
+            if (data.first_name != null) {
+                $scope.data.first_name = data.first_name;
+                $scope.user.first_name = data.first_name;
+            }
+            if (data.last_name != null) {
+                $scope.data.last_name = data.last_name;
+                $scope.user.last_name = data.last_name;
+            }
+            if (data.email != null) {
+                $scope.data.email = data.email;
+                $scope.user.email = data.email;
+            }
+        }
+
+    $scope.loadUserData = function () {
+        $http({
+            method: 'GET',
+            url: '/accounts/users/'+$scope.userid+'/'
+            }).success( function  (data) { ajaxDone(data) });
+        if ($scope.data.Logged === true)
+            $scope.is_user_data_shown = true;
+    }
+
+    $scope.changeUserData = function () {
+        $http({
+            method: 'PUT',
+            url: '/accounts/users/'+$scope.userid+'/',
+            data: angular.toJson($scope.data)
+            }).success( function  (data) { ajaxDone(data) });
+    }
+
+    $scope.hideUserData = function () {
+        $scope.is_user_data_shown = false;
+    }
+
+    getID();
 
 }
--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,22 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.http import HttpResponse
+from custom_user.models import CustomUser
+from django.test.client import Client
+import json
 
 
 class Index(TemplateView):
     template_name = "flexdb/index.html"
+
+
+class GetUserDataView(View):
+    def get(self, request, **kwargs):
+        if request.user.is_authenticated():
+
+            data = {'userid': self.request.user.id,
+                    'Logged': True}
+        else:
+            data = {'Logged': False}
+
+        data = json.dumps(data)
+        return HttpResponse(data)
