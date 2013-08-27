095dc6db1eda3f2255131dc2b865d0d486b00deb
Cezar <cezar@rome>
2013-08-27 13:38:07 +0200

    base urlopownik

--- urlopownik/static/urlopownik/js/main.js
+++ urlopownik/static/urlopownik/js/main.js
@@ -5,3 +5,21 @@ app.directive("urlopownik", function () {
         transclude: true
     }
 });
+
+
+function urlopownikControler ($scope) {
+    $scope.show = [];
+    $scope.show[0]= false;
+    $scope.show[1]= false;
+    $scope.show[2]= false;
+
+
+    $scope.showMenu = function (elementtoshow){
+        for (var i = 0; i < $scope.show.length; i++){
+            $scope.show[i]= false;
+        }
+        $scope.show[elementtoshow]= true;
+
+    }
+
+}
