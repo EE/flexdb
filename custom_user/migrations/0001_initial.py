0a874db907599b815062276770eb4f5bfcc38979
Szymon Nowicki <szymonnowicki@gmail.com>
2013-08-29 15:36:53 +0200

    added migration dependency

--- custom_user/migrations/0001_initial.py
+++ custom_user/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    needed_by = (
+        ("social_auth", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'CustomUser'
         db.create_table(u'custom_user_customuser', (
